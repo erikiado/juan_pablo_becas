8725b203218dac7e8db585028aa215a432f08ab0
erikiado <erikiado@gmail.com>
2017-02-21 15:21:37 -0600

    Change testcase for StaticLiveServerTestCase

--- base/test_views.py
+++ base/test_views.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from splinter import Browser
 
+test_url = 'http://localhost:8081/'
 
-class TestBaseViews(TestCase):
+class TestBaseViews(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = Browser('chrome')
@@ -11,17 +12,17 @@ class TestBaseViews(TestCase):
         self.browser.quit()
 
     def test_home(self):
-        self.browser.visit('http://localhost:8000')
+        self.browser.visit(test_url)
         test_string = 'Hello, world!'
         if self.browser.is_text_present(test_string):
             self.assertTrue(True)
 
     def test_robots(self):
-        self.browser.visit('http://localhost:8000/robots.txt')
+        self.browser.visit(test_url + 'robots.txt')
         if self.browser.is_text_present('robotstxt'):
             self.assertTrue(True)
 
     def test_humans(self):
-        self.browser.visit('http://localhost:8000/humans.txt')
+        self.browser.visit(test_url + 'humans.txt')
         if self.browser.is_text_present('humanstxt'):
             self.assertTrue(True)
