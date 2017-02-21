ce4d013e5852af5a39e7f4a0175663dc9c34f21a
Raul Mar <raulmar_94@hotmail.com>
2017-02-21 11:48:04 -0600

    Se modifico el objeto de la forma para crear usuario. Hay problemas para guadar a los usuarios.

--- administracion/forms.py
+++ administracion/forms.py
@@ -5,33 +5,31 @@ from django.contrib.auth.models import Group
 from perfiles_usuario.utils import ADMINISTRADOR_GROUP, CAPTURISTA_GROUP, DIRECTIVO_GROUP, \
                                    SERVICIOS_ESCOLARES_GROUP
 
+
 class FormaUsuario(forms.ModelForm):
     ROLES_USUARIO = (
-        ('ADMINISTRADOR_GROUP', ('Administrador')),
-        ('CAPTURISTA_GROUP', ('Capturista')),
-        ('DIRECTIVO_GROUP', ('Directivo')),
-        ('SERVICIOS_ESCOLARES_GROUP', ('Servicios Escolares'))
+        (ADMINISTRADOR_GROUP, ('Administrador')),
+        (CAPTURISTA_GROUP, ('Capturista')),
+        (DIRECTIVO_GROUP, ('Directivo')),
+        (SERVICIOS_ESCOLARES_GROUP, ('Servicios Escolares'))
     )
-    # first_name = forms.CharField(max_length=35, label='Nombre', required=True)
-    # last_name = forms.CharField(max_length=35, label='Apellido', required=True)
-    # email = forms.EmailField(label='Correo Electronico', required=True)
-    rol_usuario = forms.ChoiceField(choices=ROLES_USUARIO, label='Tipo de usuario', required=True)
-    # password = forms.CharField(label='Contraseña', widget=forms.PasswordInput, required=True)
+
+    # rol_usuario = forms.ChoiceField(choices=ROLES_USUARIO, label='Tipo de usuario', required=True)
 
     class Meta:
         model = get_user_model()
-        fields = ('first_name', 'last_name', 'email', 'password')
+        fields = ['first_name', 'last_name', 'email', 'password']
+        exclude = ['username',]
 
     def save(self, commit=True, *args, **kwargs):
-        # print(self)
         nuevo_usuario = super(FormaUsuario, self).save(commit=False, *args, **kwargs)
         nuevo_usuario.first_name = self.cleaned_data['first_name']
         nuevo_usuario.last_name = self.cleaned_data['last_name']
         nuevo_usuario.email = self.cleaned_data['email']
-        print(self.cleaned_data['rol_usuario'])
-        rol = Group.objects.get_or_create(self.cleaned_data['rol_usuario'])[0]
-        nuevo_usuario.groups.add(rol)
         nuevo_usuario.password = self.cleaned_data['password']
+        # rol = Group.objects.get_or_create(name=self.cleaned_data['rol_usuario'])[0]
+        # nuevo_usuario.groups.add(rol)
         if commit:
             nuevo_usuario.save()
         return nuevo_usuario
+
--- administracion/views.py
+++ administracion/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 
 from .forms import FormaUsuario
 
+
 def crear_usuario(request):
     # no se si sea necesario
     if request.method == 'POST':
@@ -10,12 +11,13 @@ def crear_usuario(request):
         forma = FormaUsuario(request.POST)
         # si la forma contiene todos los campos es valida
         if forma.is_valid():
-            first_name = forma.cleaned_data['first_name']
-            last_name   = forma.cleaned_data['last_name']
-            email       = forma.cleaned_data['email']
-            password    = forma.cleaned_data['password']
-            rol_usuario = forma.cleaned_data['rol_usuario']
-            forma.save()
+            nuevo_usuario = forma.save(commit=False)
+            nuevo_usuario.first_name = forma.cleaned_data['first_name']
+            nuevo_usuario.last_name = forma.cleaned_data['last_name']
+            nuevo_usuario.email = forma.cleaned_data['email']
+            nuevo_usuario.password = forma.cleaned_data['password']
+            # nuevo_usuario.rol_usuario = forma.cleaned_data['rol_usuario']
+            nuevo_usuario.save()
             # Direccionar a dashboard 'administracion'
             # Checar reverse de urls
             return HttpResponseRedirect('/administracion/crear_usuario/?us=1')
