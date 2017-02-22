f60c2078f27ff32e61d61ac8ed0b7529148a89dd
csssaz <alecassis16@gmail.com>
2017-02-21 18:17:12 -0600

    add namespace to urls

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import crear_usuario
 
+app_name = 'administracion'
 urlpatterns = [
     url(r'^crear_usuario/$', crear_usuario, name='crear_usuario'),
 ]
