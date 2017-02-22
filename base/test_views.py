a1deccb880c61e36a48f268a1960c4d9c0adf15c
erikiado <erikiado@gmail.com>
2017-02-21 22:08:41 -0600

    Correcciones de code review y documentacion

--- base/test_views.py
+++ base/test_views.py
@@ -1,30 +1,55 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
 from splinter import Browser
 
 
-test_url = 'http://localhost:8081/'
-
-
 class TestBaseViews(StaticLiveServerTestCase):
+    """
+    Suite de integracion para probar las rutas base.
+
+    Probar los urls de home, y los accesos a los archivos de robots y humans.
+
+    Attributes
+    ----------
+    browser : Browser
+        Driver para navegar por paginas web y para correr pruebas de integracion.
+    """
 
     def setUp(self):
+        """
+        Inicializar el navegador, antes de correr las pruebas.
+        """
         self.browser = Browser('chrome')
 
     def tearDown(self):
+        """
+        Al terminar, cerrar el navegador.
+        """
         self.browser.quit()
 
     def test_home(self):
-        self.browser.visit(test_url)
-        test_string = 'Hello, world!'
-        if self.browser.is_text_present(test_string):
-            self.assertTrue(True)
+        """
+        Prueba sobre url 'base:home'.
+
+        Visitar el url de nombre 'home' y comprobar que cargue el contenido.
+        """
+        self.browser.visit(self.live_server_url + reverse('home'))
+        self.assertTrue(self.browser.is_text_present('Hello, world!'))
 
     def test_robots(self):
-        self.browser.visit(test_url + 'robots.txt')
-        if self.browser.is_text_present('robotstxt'):
-            self.assertTrue(True)
+        """
+        Prueba sobre url 'base:base_files'(robots.txt).
+
+        Visitar el url de robots.txt y comprobar que cargue el archivo.
+        """
+        self.browser.visit(self.live_server_url + '/robots.txt')
+        self.assertTrue(self.browser.is_text_present('robotstxt'))
 
     def test_humans(self):
-        self.browser.visit(test_url + 'humans.txt')
-        if self.browser.is_text_present('humanstxt'):
-            self.assertTrue(True)
+        """
+        Prueba sobre url 'base:base_files'(humans.txt).
+
+        Visitar el url de humans.txt y comprobar que cargue el archivo.
+        """
+        self.browser.visit(self.live_server_url + '/humans.txt')
+        self.assertTrue(self.browser.is_text_present('humanstxt'))
