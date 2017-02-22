a38e3578f9f904744dc98011c632e858335cad1f
erikiado <erikiado@gmail.com>
2017-02-22 12:07:11 -0600

    Change namespace definition for administracion in main app urls to own urls

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import admin_panel_principal, admin_panel_usuarios
 
+app_name = 'administracion'
+
 urlpatterns = [
     url(r'^panel/', admin_panel_principal, name='administracion_principal'),
     url(r'^usuarios/', admin_panel_usuarios, name='administracion_usuarios')
