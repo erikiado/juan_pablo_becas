6ce7914bbfc563c1cfc15a6eb0270897c378f3a3
fernandolobato <lobato.meeser.fernando@hotmail.com>
2017-02-25 17:55:27 -0600

    Removed namespace from global urls

--- jp2_online/urls.py
+++ jp2_online/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 urlpatterns = [
     url('', include('base.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^administracion/', include('administracion.urls')),
     url(r'^tosp_auth/', include('tosp_auth.urls')),
-    url(r'^perfiles-usuario', include('perfiles_usuario.urls', namespace='perfiles_usuario')),
+    url(r'^perfiles-usuario', include('perfiles_usuario.urls')),
 ]
