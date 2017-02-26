ed26deccbeca98ab889a7288c315c5752f5dc4cd
csssaz <alecassis16@gmail.com>
2017-02-25 20:00:29 -0600

    Add model for Retroalimentación, and tests for it.

--- captura/models.py
+++ captura/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from estudios_socioeconomicos.models import Estudio
+
+class Retroalimentacion(models.Model):
+    estudio = models.ForeignKey(Estudio)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    fecha = models.DateTimeField(null=True, blank=True, auto_now_add=True)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return '{usuario} - {fecha}: {descripcion}'.format(
+                                                fecha=str(self.fecha),
+                                                usuario=str(self.usuario),
+                                                descripcion=self.descripcion[:40])
+
