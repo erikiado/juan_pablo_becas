330c54ebf6fa1a5c1cba620e0b417e8ca2836711
Lucho Acosta <luiis_acostaa@hotmail.com>
2017-02-28 08:31:19 -0600

    Fix style

--- administracion/forms.py
+++ administracion/forms.py
@@ -65,7 +65,9 @@ class FormaCreacionUsuario(FormaUsuario):
 
 
 class FormaRetroalimentacion(forms.ModelForm):
+    """ Form for creating a Retroalimentacion.
 
+    """
     class Meta:
         model = Retroalimentacion
         fields = ['estudio', 'descripcion']
