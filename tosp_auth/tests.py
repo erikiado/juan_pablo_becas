e064754c01550766534c3d0bcf086d507609e3bf
adal <adita0898@gmail.com>
2017-02-28 13:33:58 +0000

    Changes requested from travis
    Delete whitespaces, and add two blank lines.

--- tosp_auth/tests.py
+++ tosp_auth/tests.py
@@ -1,7 +1,7 @@
-from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from splinter import Browser
 from django.contrib.auth.models import User
+
 
 class WidgetLogoutTest(StaticLiveServerTestCase):
     """Integration test suite for testing the views in the app: tosp_auth.
@@ -13,25 +13,26 @@ class WidgetLogoutTest(StaticLiveServerTestCase):
         browser : Browser
             Driver to navigate through websites and to run integration tests.
     """
+
     def setUp(self):
         """Initialize the browser and create a user, before running the tests.
         """
         self.browser = Browser('chrome')
         User.objects.create_user(
             username='thelma', email='juan@pablo.com', password='junipero')
-    
+
     def tearDown(self):
         """At the end of tests, close the browser.
         """
         self.browser.quit()
-        
+
     def test_option_does_not_appear(self):
         """Test: if the option log out appear for log users
         """
         self.client.login(username='thelma', password='junipero')
         self.browser.visit(self.live_server_url)
         self.assertTrue(self.browser.is_text_present('Log Out'))
-        
+
     def test_option_appear_to_no_logged_client(self):
         """Test: if the option log out appear for not log users
         """
