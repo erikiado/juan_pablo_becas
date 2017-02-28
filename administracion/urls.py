31a5d1fc535e5934a3a3bdfd8786824c1103f7aa
erikiado <erikiado@gmail.com>
2017-02-28 09:28:49 -0600

    Add edit user to dashboard, missing tests from login

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import admin_main_dashboard, admin_users_dashboard, admin_users_create
+from .views import admin_main_dashboard, admin_users_dashboard, \
+                   admin_users_create, admin_users_edit, admin_users_edit_form
 
 app_name = 'administracion'
 
@@ -7,5 +8,7 @@ app_name = 'administracion'
 urlpatterns = [
     url(r'^principal/', admin_main_dashboard, name='main'),
     url(r'^usuarios/nuevo/', admin_users_create, name='users_add'),
+    url(r'^usuarios/editar/(\d+)/', admin_users_edit_form, name='users_edit_form'),
+    url(r'^usuarios/editar/guardar/', admin_users_edit, name='users_edit'),
     url(r'^usuarios/', admin_users_dashboard, name='users'),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test, login_required
-from .forms import FormaCreacionUsuario
+from .forms import FormaCreacionUsuario, UserUpdateForm
 from perfiles_usuario.utils import is_administrador
-
+from django.http import HttpResponse
+import json
+from django.template.loader import render_to_string
 
 @login_required
 @user_passes_test(is_administrador)
@@ -38,3 +40,41 @@ def admin_users_create(request):
         if forma.is_valid():
             forma.save()
             return redirect('administracion:users')
+
+@login_required
+@user_passes_test(is_administrador)
+def admin_users_edit_form(request, user_id):
+    """ View to send the form to edit users.
+
+    """
+    # if request.is_ajax():
+    user = User.objects.get(pk=user_id)
+    form = UserUpdateForm(instance=user,initial={'rol_usuario': user.groups.all()[0].name})
+    return render(request,'administracion/user_form.html',{'user_form':form, 'from_user':user})
+
+@login_required
+@user_passes_test(is_administrador)
+def admin_users_edit(request):
+    """ View to edit users.
+
+    """
+    # user = User.objects.get(pk=1)
+    # print(request)
+    if request.method == 'POST':
+        user_id = request.POST['user_id']
+        instance = User.objects.get(pk=user_id)
+        form = UserUpdateForm(request.POST or None, instance=instance)
+        if form.is_valid():
+            form.save()
+        return redirect('administracion:users')
+
+    # print(user)
+        # user_form = {}
+        # user_form["errors"] = form.errors
+        # user_form["fields"] = {}
+        # for field in form:
+            # user_form["fields"][field] = field
+        # html = render_to_string('administracion/user_form.html', {'user_form': user_form}, request=request)
+        # if forma.is_valid():
+            # forma.save()
+        # return HttpResponse(json.dumps({'name': forma}), content_type="application/json")
