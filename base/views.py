b3d45de87619b004bf0f5db9cb5df6d981dc1801
csssaz <alecassis16@gmail.com>
2017-03-04 20:51:41 -0600

    add render of home for users w/o group

--- base/views.py
+++ base/views.py
@@ -7,6 +7,11 @@ from perfiles_usuario.utils import is_administrador, is_capturista,\
 
 @login_required
 def home(request):
+    """ This view redirects users according to their group.
+
+    TODO:
+    Figure out what to do w/users that don't have groups assigned.
+    """
     if is_administrador(request.user):
         return redirect('administracion:main')
     if is_capturista(request.user):
@@ -15,8 +20,9 @@ def home(request):
         return redirect('indicadores:all')
     if is_servicios_escolares(request.user):
         return redirect('becas:services')
+    return render(request, 'base/home.html')
 
 
 def base_files(request, filename):
-    location = "base/" + filename
-    return render(request, location, {}, content_type="text/plain")
+    location = 'base/' + filename
+    return render(request, location, {}, content_type='text/plain')
