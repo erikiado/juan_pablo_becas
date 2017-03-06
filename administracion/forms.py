0a9e08b1e03a7377487e5d3ffba7f3317a4dbb96
erikiado <erikiado@gmail.com>
2017-03-06 07:42:57 -0600

    Update user form to update the user.

--- administracion/forms.py
+++ administracion/forms.py
@@ -6,7 +6,10 @@ from perfiles_usuario.utils import ADMINISTRADOR_GROUP, CAPTURISTA_GROUP, DIRECT
                                    SERVICIOS_ESCOLARES_GROUP
 from perfiles_usuario.models import Capturista
 
+<<<<<<< 8431ca4c9c0a2170a301e18e231376ae5a2b6944
 
+=======
+>>>>>>> Update user form to update the user.
 class UserModelForm(forms.ModelForm):
     """ ModelForm for Users.
 
@@ -41,6 +44,7 @@ class UserForm(UserModelForm):
 
     rol_usuario = forms.ChoiceField(choices=ROLES_USUARIO, label='Tipo de usuario', required=True)
 
+<<<<<<< 8431ca4c9c0a2170a301e18e231376ae5a2b6944
     def generate_user_password(self):
         return self.instance.first_name+'_'+self.instance.last_name
 
@@ -96,35 +100,11 @@ class UserForm(UserModelForm):
 
 class UserUpdateForm(FormaCreacionUsuario):
     """ Form for creating different types of users.
+=======
+>>>>>>> Update user form to update the user.
 
-    This form inherits from FormaUsuario, and is meant to be used for
-    the creation of any kind of user by the administrative.
-
-    The save method is overriden to add the corresponding group to the created user.
-    """
-
-
-    # def __init__(self, *args, **kwargs):
-
-        # initial =  kwargs.get('initial', {})
-        # rol_usuario = initial.get('rol_usuario', None)
-
-        # set just the initial value
-        # in the real form needs something like this {'choice':'a'}
-        # but in this case you want {'choice':('a', 'letter_a')}
-        # if rol_usuario:
-            # kwargs['initial']['rol_usuario'] = kwargs['instance'].groups.all()[0]
-        # super(UserUpdateForm, self).__init__(*args, **kwargs)
-        # print(kwargs['instance'].groups.all()[0].name)
-
-        # self.fields['rol_usuario'] = kwargs['instance'].groups.all()[0].name
-
-    password = forms.CharField(required=False, widget=forms.PasswordInput)
-
-    class Meta(FormaUsuario.Meta):
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+    def generate_user_password(self):
+        return self.instance.first_name+'_'+self.instance.last_name
 
     def save(self, *args, **kwargs):
         """ Override save method to add group to the user.
@@ -133,21 +113,45 @@ class UserUpdateForm(FormaCreacionUsuario):
         add the corresponding group to the user that is being created.
         The group is chosen based on the ChoiceField defined above
         """
-        user = super(FormaCreacionUsuario, self).save(*args, **kwargs)
         data = self.cleaned_data
-        # print(data)
-        # print(user)
-        if user.groups.all()[0].name == CAPTURISTA_GROUP:
-            print(user)
-        else:
-            print("NEL")
+        # Create user
+        if self.instance.pk is None:
+            user = super(UserForm, self).save(*args, **kwargs)
+            # TODO: Change the password generation for an PasswordResetForm
+            user.set_password(self.generate_user_password())
+            if data['rol_usuario'] == CAPTURISTA_GROUP:
+                user.save()
+                # capturista's group is added in the save method of the Model.
+                capturista = Capturista(user=user)
+                capturista.save()
+            else:
+                user_group = Group.objects.get_or_create(name=data['rol_usuario'])[0]
+                user.groups.add(user_group)
+                user.save()
 
-        # if data['rol_usuario'] == CAPTURISTA_GROUP:
-        #     # capturista's group is added in the save method of the Model.
-        #     capturista = Capturista(user=user)
-        #     capturista.save()
-        # else:
-        #     user_group = Group.objects.get_or_create(name=data['rol_usuario'])[0]
-        #     user.groups.add(user_group)
-        #     user.save()
+        # Update user
+        else:
+            user = self.instance
+            user.username = data['username']
+            user.first_name = data['first_name']
+            user.last_name = data['last_name']
+            user.email = data['email']
+
+            user_group_name = user.groups.all()[0].name
+
+            if data['rol_usuario'] != user_group_name:
+                user.groups.clear()
+                if data['rol_usuario'] == CAPTURISTA_GROUP:
+                    user.save()
+                    capturista = Capturista(user=user)
+                    capturista.save()
+                else:
+                    if user_group_name == CAPTURISTA_GROUP:
+                        capturista = Capturista.objects.get(user=user)
+                        capturista.delete()
+                    user_group = Group.objects.get_or_create(name=data['rol_usuario'])[0]
+                    user.groups.add(user_group)
+                    user.save()
+            else:
+                user.save()
         return user
