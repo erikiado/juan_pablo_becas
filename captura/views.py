e8f9ae6121beac88757a16dd58a05bc84b23aaa1
a01204421 <a01204421@itesm.mx>
2017-03-06 18:22:31 -0600

    Create view to dashboard capturista

--- captura/views.py
+++ captura/views.py
@@ -1,3 +1,15 @@
-# from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.shortcuts import render
+from perfiles_usuario.utils import is_capturista
+from estudios_socioeconomicos.models import Estudio
 
-# Create your views here.
+
+@login_required
+@user_passes_test(is_capturista)
+def capturista_dashboard(request):
+    """View to render the capturista control dashboard.
+
+    """
+    estudios = Estudio.objects.filter(status='rechazado')
+    return render(request, 'captura/dashboard_capturista.html',
+                  {'estudios': estudios})
