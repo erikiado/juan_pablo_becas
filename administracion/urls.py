b006d96e7cfb419922bb603d1238402457aba449
csssaz <alecassis16@gmail.com>
2017-04-06 20:45:00 -0500

    basic view to render details of a student

--- administracion/urls.py
+++ administracion/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import admin_main_dashboard, admin_users_dashboard, \
                    admin_users_create, admin_users_edit, admin_users_edit_form, \
                    admin_users_delete_modal, admin_users_delete, list_studies, \
-                   focus_mode, reject_study, search_students
+                   focus_mode, reject_study, search_students, detail_student
 
 app_name = 'administracion'
 
@@ -19,4 +19,5 @@ urlpatterns = [
     url(r'^principal/(?P<study_id>[\w\-]+)/detalle', focus_mode, name='focus_mode'),
     url(r'^estudio/rechazar/$', reject_study, name='reject_study'),
     url(r'^busqueda/', search_students, name='search_students'),
+    url(r'^detalle-alumno/(?P<id_alumno>[0-9]+)', detail_student, name='detail_student'),
 ]
