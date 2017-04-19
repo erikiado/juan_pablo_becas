8be34c31b66baf5bc115829f704a2d5c58ac6831
csssaz <alecassis16@gmail.com>
2017-04-18 19:44:16 -0500

    add view and test for it

--- becas/tests.py
+++ becas/tests.py
@@ -1,3 +1,79 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User, Group
 
-# Create your tests here.
+from perfiles_usuario.models import Capturista
+from perfiles_usuario.utils import ADMINISTRADOR_GROUP
+from administracion.models import Escuela
+from estudios_socioeconomicos.models import Estudio
+from familias.models import Familia, Integrante, Alumno
+
+
+class TestViewsBecas(TestCase):
+    """Unit test suite for testing the views in the app: administracion.
+
+    Test that the views for 'administracion' are correctly received as a response and that
+    they use the correct template.
+    """
+
+    def setUp(self):
+        """ This generates the family with its integrantes and
+            a estudio that will be later queried in the focus mode.
+        """
+        thelma = User.objects.create_user(
+            username='thelma', email='juan@pablo.com', password='junipero',
+            first_name='Thelma', last_name='Amlet')
+        administrators = Group.objects.get_or_create(name=ADMINISTRADOR_GROUP)[0]
+        administrators.user_set.add(thelma)
+        self.client.login(username='thelma', password='junipero')
+
+        elerik = User.objects.create_user(
+            username='erikiano',
+            email='latelma@junipero.sas',
+            password='eugenio420',
+            first_name='telma',
+            last_name='suapellido')
+
+        self.capturista = Capturista.objects.create(user=elerik)
+        self.capturista.save()
+
+        self.familia = Familia.objects.create(
+            numero_hijos_diferentes_papas=3,
+            explicacion_solvencia='narco',
+            estado_civil='secreto',
+            localidad='otro')
+
+        self.estudio = Estudio.objects.create(
+            capturista=self.capturista,
+            familia=self.familia,
+            status=Estudio.APROBADO)
+
+        self.escuela = Escuela.objects.create(nombre='Rolando Calles')
+
+        self.integrante_alumno = Integrante.objects.create(
+            familia=self.familia,
+            nombres='Rick',
+            apellidos='Astley',
+            nivel_estudios='doctorado',
+            fecha_de_nacimiento='1996-02-26')
+
+        self.alumno = Alumno.objects.create(
+            integrante=self.integrante_alumno,
+            escuela=self.escuela)
+
+    def test_template_asignar_beca(self):
+        """ Test that we can access the url and
+        the template is the expected one.
+        """
+        response = self.client.get(reverse('becas:asignar_beca',
+                                           kwargs={'id_estudio': self.estudio.id}))
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'becas/asignar_beca.html')
+
+    def test_asignar_becas_invalid_study(self):
+        """ Test that we get a 404 if the id of the study is invalid
+        in the url.
+        """
+        response = self.client.get(reverse('becas:asignar_beca',
+                                           kwargs={'id_estudio': 101010}))
+        self.assertEqual(404, response.status_code)
--- becas/views.py
+++ becas/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import user_passes_test, login_required
+
+from familias.utils import total_egresos_familia, total_ingresos_familia, total_neto_familia
+from perfiles_usuario.utils import is_administrador
+from estudios_socioeconomicos.models import Estudio
 
 
 @login_required
@@ -12,3 +16,20 @@ def estudios(request):
     TODO: name properly and implement everything
     """
     return render(request, 'layouts/dashboard_base.html')
+
+
+@login_required
+@user_passes_test(is_administrador)
+def asignar_beca(request, id_estudio):
+    """ Renders the view where the admin assigns the scolarship
+    to a family after approving a study.
+    """
+    estudio = get_object_or_404(Estudio, pk=id_estudio)
+    context = {}
+    context['estudio'] = estudio
+
+    context['total_egresos_familia'] = total_egresos_familia(estudio.familia.id)
+    context['total_ingresos_familia'] = total_ingresos_familia(estudio.familia.id)
+    context['total_neto_familia'] = total_neto_familia(estudio.familia.id)
+
+    return render(request, 'becas/asignar_beca.html', context)
