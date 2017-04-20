28d4475d222ccffbdde7f5018e5e3bcb1da394eb
csssaz <alecassis16@gmail.com>
2017-04-20 12:21:49 -0500

    improve look and add tests

--- becas/views.py
+++ becas/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import user_passes_test, login_required
+from django.http.response import HttpResponseBadRequest
 
 from familias.utils import total_egresos_familia, total_ingresos_familia, total_neto_familia
+from familias.models import Integrante
 from perfiles_usuario.utils import is_administrador
 from estudios_socioeconomicos.models import Estudio, Foto
 
 from .forms import BecaForm
+from .models import Beca
 
 
 @login_required
@@ -30,13 +33,27 @@ def asignar_beca(request, id_estudio):
     """
     estudio = get_object_or_404(Estudio, pk=id_estudio)
     fotos = Foto.objects.filter(estudio=id_estudio)
-
     context = {
         'estudio': estudio,
         'total_egresos_familia': total_egresos_familia(estudio.familia.id),
         'total_ingresos_familia': total_ingresos_familia(estudio.familia.id),
         'total_neto_familia': total_neto_familia(estudio.familia.id),
-        'fotos': fotos,
-        'form': BecaForm()
+        'fotos': fotos
     }
-    return render(request, 'becas/asignar_beca.html', context)
+    if request.method == 'GET':
+        context['form'] = BecaForm()
+        return render(request, 'becas/asignar_beca.html', context)
+    elif request.method == 'POST':
+        form = BecaForm(request.POST)
+        if form.is_valid():
+            percentage = float(form.cleaned_data['porcentaje']) / 100.
+            # create scolarships for active students
+            integrantes = Integrante.objects.filter(familia__pk=estudio.familia.pk, activo=True)
+            for integrante in filter(lambda x: hasattr(x, 'alumno_integrante'), integrantes):
+                Beca.objects.create(alumno=integrante.alumno_integrante,
+                                    monto=1500. - (1500. * percentage))
+            return redirect('becas:asignar_beca', id_estudio=id_estudio)
+        else:
+            context['form'] = form
+            return render(request, 'becas/asignar_beca.html', context)
+    return HttpResponseBadRequest()
