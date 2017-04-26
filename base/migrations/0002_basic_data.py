bb5079636c50bad15e4b87e2939233e97232f8ec
Perimetro20 <a01205778@itesm.mx>
2017-04-25 20:45:28 -0500

    Fix spelling mistakes

--- base/migrations/0002_basic_data.py
+++ base/migrations/0002_basic_data.py
@@ -10,25 +10,25 @@ def forwards_func(apps, schema_editor):
     db_alias = schema_editor.connection.alias
     Tutorial.objects.using(db_alias).bulk_create([
         Tutorial(titulo='¿Cómo hacer login por primera vez?',
-                 descripcion='En este tutorial se muestra como hacer login una vez que el administrador crea tu cuenta.',
+                 descripcion='En este tutorial se muestra cómo hacer login una vez que el administrador crea tu cuenta.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='general'),
         Tutorial(titulo='¿Cómo hacer login a la página?',
-                 descripcion='En este tutorial se muestra como entrar a tu cuenta.',
+                 descripcion='En este tutorial se muestra cómo entrar a tu cuenta.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='general'),
         Tutorial(titulo='¿Cómo hacer logout de la página?',
-                 descripcion='En este tutorial se muestra como cerrar tu sesión.',
+                 descripcion='En este tutorial se muestra cómo cerrar tu sesión.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='general'),
         Tutorial(titulo='¿Cómo recuperar tu contraseña?',
-                 descripcion='En este tutorial se muestra como recuparar tu contraseña, en caso de que sea olvidada.',
+                 descripcion='En este tutorial se muestra cómo recuparar tu contraseña, en caso de que sea olvidada.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='general'),
 
         # Carga de sección de administración
         Tutorial(titulo='¿Cómo crear un nuevo usuario?',
-                 descripcion='En este tutorial se muestra como crear un nuevo usuario.',
+                 descripcion='En este tutorial se muestra cómo crear un nuevo usuario.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='administracion'),
         Tutorial(titulo='¿Cómo editar un nuevo usuario?',
@@ -48,15 +48,15 @@ def forwards_func(apps, schema_editor):
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='administracion'),
         Tutorial(titulo='¿Cómo retroalimentar un estudio?',
-                 descripcion='En este tutorial se muestra como rechazar y retroalimentar un estudio.',
+                 descripcion='En este tutorial se muestra cómo rechazar y retroalimentar un estudio.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='administracion'),
-        Tutorial(titulo='¿Cómo aprovar un estudio?',
-                 descripcion='En este tutorial se muestra como aprobar un estudio y los cambios que genera.',
+        Tutorial(titulo='¿Cómo aprobar un estudio?',
+                 descripcion='En este tutorial se muestra cómo aprobar un estudio y los cambios que genera.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='administracion'),
         Tutorial(titulo='¿Cómo asignar una beca?',
-                 descripcion='En este tutorial se muestra como se le otorga una beca a la familia.',
+                 descripcion='En este tutorial se muestra cómo se le otorga una beca a la familia.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='administracion'),
         Tutorial(titulo='¿Cómo reasignar una beca?',
@@ -64,53 +64,53 @@ def forwards_func(apps, schema_editor):
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='administracion'),
         Tutorial(titulo='¿Cómo generar la carta de beca de un niño?',
-                 descripcion='En este tutorial se muestra como crear la carta de beca para un alumno.',
+                 descripcion='En este tutorial se muestra cómo crear la carta de beca para un alumno.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='administracion'),
 
         # Carga de sección de capturista
         Tutorial(titulo='¿Creación de un nuevo estudio socioeconómico?',
-                 descripcion='En este tutorial se muestra como crear un nuevo estudio socioeconómico.',
+                 descripcion='En este tutorial se muestra cómo crear un nuevo estudio socioeconómico.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
         Tutorial(titulo='¿Cómo llenar al sección de integrantes?',
-                 descripcion='En este tutorial se muestra como llenar la sección de integrantes.',
+                 descripcion='En este tutorial se muestra cómo llenar la sección de integrantes.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
         Tutorial(titulo='¿Cómo llenar los egresos e ingresos?',
-                 descripcion='En este tutorial se muestra como desglosar los egresos e ingresos de la familia.',
+                 descripcion='En este tutorial se muestra cómo desglosar los egresos e ingresos de la familia.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
         Tutorial(titulo='¿Cómo subir las imágenes de la vivienda?',
-                 descripcion='En este tutorial se muestra como subir las imágenes tomadas de la vivienda.',
+                 descripcion='En este tutorial se muestra cómo subir las imágenes tomadas de la vivienda.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
         Tutorial(titulo='¿Cómo llenar el resto del estudio socioeconómico?',
-                 descripcion='En este tutorial se muestra como llenar el resto de la información de la familia.',
+                 descripcion='En este tutorial se muestra cómo llenar el resto de la información de la familia.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
         Tutorial(titulo='¿Cómo subir el estudio a revisión?',
                  descripcion='En este tutorial se muestra cómo subir un estudio socioeconómico, y que hay que checar antes de subirlo.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
-        Tutorial(titulo='¿Cómo lidear con el proceso de retroalimentación?',
-                 descripcion='En este tutorial se explica como funciona el proceso de retroalimentación',
+        Tutorial(titulo='¿Cómo lidiar con el proceso de retroalimentación?',
+                 descripcion='En este tutorial se explica cómo funciona el proceso de retroalimentación',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
-        Tutorial(titulo='¿Que significan los diferentes estados del estudio socioeconómico?',
+        Tutorial(titulo='¿Qué significan los diferentes estados del estudio socioeconómico?',
                  descripcion='En este tutorial se explica cada uno de los estados y su significado.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='captura'),
 
         # Carga de sección dirección
         Tutorial(titulo='¿Cómo descargar el resumen completo de información de la plataforma?',
-                 descripcion='En este tutorial se muestra como descargar un resumen total de los datos de la escuela.',
+                 descripcion='En este tutorial se muestra cómo descargar un resumen total de los datos de la escuela.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='direccion'),
 
         # Carga de sección de servicios escolares
         Tutorial(titulo='¿Cómo marcar un alumno que ha empezado el proceso de reinscripción?',
-                 descripcion='En este tutorial se muestra como marcar que un alumno ha empezado el proceso de reinscripción y requiere una carta de beca.',
+                 descripcion='En este tutorial se muestra cómo marcar que un alumno ha empezado el proceso de reinscripción y requiere una carta de beca.',
                  video='https://www.youtube.com/embed/dQw4w9WgXcQ',
                  seccion='servicios escolares'),
         ])
@@ -134,7 +134,7 @@ def reverse_func(apps, schema_editor):
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo buscar a un niño?').delete()
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo buscar a una familia?').delete()
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo retroalimentar un estudio?').delete()
-    Tutorial.objects.using(db_alias).filter(titulo='¿Cómo aprovar un estudio?').delete()
+    Tutorial.objects.using(db_alias).filter(titulo='¿Cómo aprobar un estudio?').delete()
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo asignar una beca?').delete()
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo reasignar una beca?').delete()
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo generar la carta de beca de un niño?').delete()
@@ -146,8 +146,8 @@ def reverse_func(apps, schema_editor):
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo subir las imágenes de la vivienda?').delete()
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo llenar el resto del estudio socioeconómico?').delete()
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo subir el estudio a revisión?').delete()
-    Tutorial.objects.using(db_alias).filter(titulo='¿Cómo lidear con el proceso de retroalimentación?').delete()
-    Tutorial.objects.using(db_alias).filter(titulo='¿Que significan los diferentes estados del estudio socioeconómico?').delete()
+    Tutorial.objects.using(db_alias).filter(titulo='¿Cómo lidiar con el proceso de retroalimentación?').delete()
+    Tutorial.objects.using(db_alias).filter(titulo='¿Qué significan los diferentes estados del estudio socioeconómico?').delete()
 
     # Borrado de sección de dirección
     Tutorial.objects.using(db_alias).filter(titulo='¿Cómo descargar el resumen completo de información de la plataforma?').delete()
--- base/test_migrations.py
+++ base/test_migrations.py
@@ -8,8 +8,7 @@ class TestLoadTutorials(TestCase):
     """
 
     def test_tutorials_created(self):
-        """ Checks that this method __str__ method returns the name
-        of the object.
+        """ Checks that all the tutorials where properly created.
         """
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo hacer login por primera vez?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo hacer login por primera vez?'))
@@ -23,7 +22,7 @@ class TestLoadTutorials(TestCase):
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo buscar a una familia?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo editar un nuevo usuario?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo retroalimentar un estudio?'))
-        self.assertTrue(Tutorial.objects.get(titulo='¿Cómo aprovar un estudio?'))
+        self.assertTrue(Tutorial.objects.get(titulo='¿Cómo aprobar un estudio?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo asignar una beca?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo reasignar una beca?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo generar la carta de beca de un niño?'))
@@ -33,7 +32,7 @@ class TestLoadTutorials(TestCase):
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo subir las imágenes de la vivienda?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo llenar el resto del estudio socioeconómico?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo subir el estudio a revisión?'))
-        self.assertTrue(Tutorial.objects.get(titulo='¿Cómo lidear con el proceso de retroalimentación?'))
-        self.assertTrue(Tutorial.objects.get(titulo='¿Que significan los diferentes estados del estudio socioeconómico?'))
+        self.assertTrue(Tutorial.objects.get(titulo='¿Cómo lidiar con el proceso de retroalimentación?'))
+        self.assertTrue(Tutorial.objects.get(titulo='¿Qué significan los diferentes estados del estudio socioeconómico?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo descargar el resumen completo de información de la plataforma?'))
         self.assertTrue(Tutorial.objects.get(titulo='¿Cómo marcar un alumno que ha empezado el proceso de reinscripción?'))
