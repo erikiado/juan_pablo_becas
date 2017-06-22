fb26c402c433ac3358dbaadfb71762cfaf506a34
erikiado <erikiado@gmail.com>
2017-06-21 21:46:38 -0500

    Add subdomain for educacion integral

--- jp2_online/settings/production.py
+++ jp2_online/settings/production.py
@@ -4,8 +4,14 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['138.197.197.47', 'junipero.erikiado.com']
+ALLOWED_HOSTS = [
+    '138.197.197.47',
+    'junipero.erikiado.com',
+    'basededatos.educacionintegral.org']
 
-CORS_ORIGIN_WHITELIST = ('138.197.197.47', 'junipero.erikiado.com')
+CORS_ORIGIN_WHITELIST = (
+    '138.197.197.47',
+    'junipero.erikiado.com',
+    'basededatos.educacionintegral.org')
 
 STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
