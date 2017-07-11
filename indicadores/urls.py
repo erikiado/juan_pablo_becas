a293e21ba06b32443513c3de6c30b0263f93253e
Perimetro20 <a01205778@itesm.mx>
2017-07-11 10:17:32 -0500

    Partial progress

--- indicadores/urls.py
+++ indicadores/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import all_indicadores
+from .views import all_indicadores, specific_indicador
 
 app_name = 'indicadores'
 
 # Urls en espanol
 urlpatterns = [
     url(r'^todos/', all_indicadores, name='all'),
+    url(r'^(?P<indicador>\w+)/', specific_indicador, name='specific_indicador')
 ]
--- indicadores/views.py
+++ indicadores/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .calculators import estado_civil_counter, numero_de_alumnos
 
+
+def f(x):
+    return {
+        'estado_civil': estado_civil_counter(),
+        'numero_de_alumnos': numero_de_alumnos(),
+    }.get(x, 'Indicador Inválido')
 
 @login_required
 def all_indicadores(request):
@@ -11,4 +18,17 @@ def all_indicadores(request):
 
     TODO: name properly and implement everything
     """
-    return render(request, 'layouts/dashboard_base.html')
+
+    data_estado_civil = estado_civil_counter()
+    context = {'data': data_estado_civil}
+    return render(request, 'indicadores/charts.html', context)
+
+@login_required
+def specific_indicador(request, indicador):
+    """ View for presenting a specific indicador, specified in the
+    url.
+
+    """
+    context = {'data': f(indicador),
+               'titulo': indicador}
+    return render(request, 'indicadores/charts.html', context)
